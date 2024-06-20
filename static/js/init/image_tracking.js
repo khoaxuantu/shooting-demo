2a2bca3ceb2e5665128ecf8ab73e1db780225338
khoaxuantu <ngxktuzkai2000@gmail.com>
2024-06-21 00:35:19 +0700

    chore: turn on resize again to fullscreen

--- static/js/init/image_tracking.js
+++ static/js/init/image_tracking.js
@@ -48,9 +48,9 @@ const arToolkitSource = new THREEx.ArToolkitSource({
 
 arToolkitSource.init(function onReady() {
   // use a resize to fullscreen mobile devices
-  // setTimeout(function () {
-  //   onResize();
-  // }, 1000);
+  setTimeout(function () {
+    onResize();
+  }, 1000);
 });
 
 // handle resize
